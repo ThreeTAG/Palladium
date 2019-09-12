f493682d31afc527716e4256bdfd519701968433
Lucraft <crafter.l2012@gmail.com>
2019-09-12 19:56:34 +0200

    Removed old recipe json automation

--- src/main/java/net/threetag/threecore/ThreeCore.java
+++ src/main/java/net/threetag/threecore/ThreeCore.java
@@ -1,23 +1,26 @@
 package net.threetag.threecore;
 
-import net.threetag.threecore.addonpacks.ThreeCoreAddonPacks;
-import net.threetag.threecore.abilities.ThreeCoreAbilities;
-import net.threetag.threecore.base.ThreeCoreBase;
-import net.threetag.threecore.karma.ThreeCoreKarma;
-import net.threetag.threecore.sizechanging.ThreeCoreSizeChanging;
-import net.threetag.threecore.util.SupporterHandler;
-import net.threetag.threecore.util.client.RenderUtil;
-import net.threetag.threecore.util.recipe.RecipeUtil;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
+import net.threetag.threecore.abilities.ThreeCoreAbilities;
+import net.threetag.threecore.addonpacks.ThreeCoreAddonPacks;
+import net.threetag.threecore.base.ThreeCoreBase;
+import net.threetag.threecore.karma.ThreeCoreKarma;
+import net.threetag.threecore.sizechanging.ThreeCoreSizeChanging;
+import net.threetag.threecore.util.SupporterHandler;
+import net.threetag.threecore.util.client.RenderUtil;
+import net.threetag.threecore.util.data.ThreeCoreBlockTagsProvider;
+import net.threetag.threecore.util.data.ThreeCoreItemTagsProvider;
+import net.threetag.threecore.util.data.ThreeCoreRecipeProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -35,7 +38,7 @@ public class ThreeCore {
 
     public ThreeCore() {
         // Basic stuff
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(RecipeUtil::gatherData);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::gatherData);
         SupporterHandler.load();
         SupporterHandler.enableSupporterCheck();
 
@@ -51,6 +54,12 @@ public class ThreeCore {
 
         // Misc
         MinecraftForge.EVENT_BUS.addListener(RenderUtil::onRenderGlobal);
+    }
+
+    public void gatherData(GatherDataEvent e) {
+        e.getGenerator().addProvider(new ThreeCoreBlockTagsProvider(e.getGenerator()));
+        e.getGenerator().addProvider(new ThreeCoreItemTagsProvider(e.getGenerator()));
+        e.getGenerator().addProvider(new ThreeCoreRecipeProvider(e.getGenerator()));
     }
 
     public static <MSG> int registerMessage(Class<MSG> messageType, BiConsumer<MSG, PacketBuffer> encoder, Function<PacketBuffer, MSG> decoder, BiConsumer<MSG, Supplier<NetworkEvent.Context>> messageConsumer) {
