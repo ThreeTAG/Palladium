d7e2c1db788d34c44d736844ff0432fec1cacdf5
Lucraft <crafter.l2012@gmail.com>
2019-10-19 20:25:30 +0200

    Made energy conduits waterloggable

--- src/main/java/net/threetag/threecore/base/block/EnergyConduitBlock.java
+++ src/main/java/net/threetag/threecore/base/block/EnergyConduitBlock.java
@@ -3,11 +3,17 @@ package net.threetag.threecore.base.block;
 import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.IStringSerializable;
@@ -33,10 +39,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.IntSupplier;
 
-public class EnergyConduitBlock extends Block {
+public class EnergyConduitBlock extends Block implements IWaterLoggable {
 
     private static final Direction[] FACING_VALUES = Direction.values();
-    private static final ConduitConnection[] CONNECTION_VALUES = ConduitConnection.values();
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final EnumProperty<ConduitConnection> NORTH = EnumProperty.create("north", ConduitConnection.class);
     public static final EnumProperty<ConduitConnection> EAST = EnumProperty.create("east", ConduitConnection.class);
     public static final EnumProperty<ConduitConnection> SOUTH = EnumProperty.create("south", ConduitConnection.class);
@@ -59,7 +65,7 @@ public class EnergyConduitBlock extends Block {
         super(properties);
         this.type = type;
         this.shapes = this.makeShapes(apothem);
-        this.setDefaultState(this.stateContainer.getBaseState().with(NORTH, ConduitConnection.NONE).with(EAST, ConduitConnection.NONE).with(SOUTH, ConduitConnection.NONE).with(WEST, ConduitConnection.NONE).with(UP, ConduitConnection.NONE).with(DOWN, ConduitConnection.NONE));
+        this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, false).with(NORTH, ConduitConnection.NONE).with(EAST, ConduitConnection.NONE).with(SOUTH, ConduitConnection.NONE).with(WEST, ConduitConnection.NONE).with(UP, ConduitConnection.NONE).with(DOWN, ConduitConnection.NONE));
     }
 
     @Override
@@ -135,10 +141,21 @@ public class EnergyConduitBlock extends Block {
 
     @Override
     public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        if (stateIn.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+
         return makeConnections(worldIn, currentPos);
     }
 
+    @Override
+    public IFluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
     public BlockState makeConnections(IBlockReader blockReader, BlockPos pos) {
+        IFluidState fluidState = blockReader.getFluidState(pos);
+        boolean waterLogged = fluidState.isTagged(FluidTags.WATER) && fluidState.getLevel() == 8;
         ConduitConnection down = getConnection(blockReader, pos, Direction.DOWN);
         ConduitConnection up = getConnection(blockReader, pos, Direction.UP);
         ConduitConnection north = getConnection(blockReader, pos, Direction.NORTH);
@@ -146,7 +163,7 @@ public class EnergyConduitBlock extends Block {
         ConduitConnection east = getConnection(blockReader, pos, Direction.EAST);
         ConduitConnection west = getConnection(blockReader, pos, Direction.WEST);
         ConduitDirection direction = getDirection(north.hasConnection(), south.hasConnection(), east.hasConnection(), west.hasConnection(), up.hasConnection(), down.hasConnection());
-        return this.getDefaultState().with(DIRECTION, direction).with(DOWN, down).with(UP, up).with(NORTH, north).with(EAST, east).with(SOUTH, south).with(WEST, west);
+        return this.getDefaultState().with(WATERLOGGED, waterLogged).with(DIRECTION, direction).with(DOWN, down).with(UP, up).with(NORTH, north).with(EAST, east).with(SOUTH, south).with(WEST, west);
     }
 
     public static ConduitConnection getConnection(IBlockReader reader, BlockPos pos, Direction direction) {
@@ -174,7 +191,7 @@ public class EnergyConduitBlock extends Block {
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, DIRECTION);
+        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, DIRECTION, WATERLOGGED);
     }
 
     public enum ConduitConnection implements IStringSerializable {
