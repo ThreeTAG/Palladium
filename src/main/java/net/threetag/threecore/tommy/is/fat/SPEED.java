c822fedfbbfe292ab159cead77dc4e16e5258806
Swirtzly <sandedshoes@gmail.com>
2019-12-28 20:02:05 +0000

    Added Speedsters.

---
+++ src/main/java/net/threetag/threecore/tommy/is/fat/SPEED.java
@@ -0,0 +1,26 @@
+package net.threetag.threecore.tommy.is.fat;
+
+/**
+ * Created by Swirtzly
+ * on 28/12/2019 @ 19:57
+ */
+public class SPEED {
+
+    public static void INUT(){
+        ADDSPEED("FLESH");
+        ADDSPEED("SOLOMAN");
+        ADDSPEED("SAVEMYTAR");
+        ADDSPEED("COSCO");
+        ADDSPEED("MOLLY EAST");
+        ADDSPEED("FLASH REVERSE");
+        ADDSPEED("FLASH SPIDER");
+        ADDSPEED("FLASHY HULK"); //code stripper ai
+        ADDSPEED("HEATBLAST"); //PEOPLE CALL HIM FIRESTORM FOR SOME REASON?
+    }
+
+    public static void ADDSPEED(String speedster) {
+        minecreft.getUnstance().addSpanker(speedster);
+    }
+
+
+}
