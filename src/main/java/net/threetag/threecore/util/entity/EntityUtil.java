e3464d2366e2c3acd0221a05759d3b825a4275ee
Lucas <lucasbandermann@gmail.com>
2020-01-12 15:31:49 +0100

    Added ray tracing method to EntityUtil

--- src/main/java/net/threetag/threecore/util/entity/EntityUtil.java
+++ src/main/java/net/threetag/threecore/util/entity/EntityUtil.java
@@ -1,8 +1,8 @@
 package net.threetag.threecore.util.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ExperienceOrbEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 
 public class EntityUtil {
@@ -24,6 +24,34 @@ public class EntityUtil {
             i = ExperienceOrbEntity.getXPSplit(amount);
             amount -= i;
             world.addEntity(new ExperienceOrbEntity(world, x, y, z, i));
+        }
+    }
+
+    public static RayTraceResult rayTraceWithEntities(Entity entityIn, double distance, RayTraceContext.BlockMode blockModeIn, RayTraceContext.FluidMode fluidModeIn) {
+        Vec3d lookVec = entityIn.getLookVec();
+        Vec3d startVec = entityIn.getPositionVec().add(0, entityIn.getEyeHeight(), 0);
+        Vec3d endVec = startVec.add(entityIn.getLookVec().scale(distance));
+        RayTraceResult blockResult = entityIn.world.rayTraceBlocks(new RayTraceContext(startVec, endVec, blockModeIn, fluidModeIn, entityIn));
+        RayTraceResult entityResult = null;
+
+        for (int i = 0; i < distance * 2; i++) {
+            if (entityResult != null)
+                break;
+            float scale = i / 2F;
+            Vec3d pos = startVec.add(lookVec.scale(scale));
+
+            Vec3d min = pos.add(0.25F, 0.25F, 0.25F);
+            Vec3d max = pos.add(-0.25F, -0.25F, -0.25F);
+            for (Entity entity : entityIn.world.getEntitiesWithinAABBExcludingEntity(entityIn, new AxisAlignedBB(min.x, min.y, min.z, max.x, max.y, max.z))) {
+                entityResult = new EntityRayTraceResult(entity, pos);
+                break;
+            }
+        }
+
+        if (entityResult != null && entityResult.getHitVec().distanceTo(startVec) <= blockResult.getHitVec().distanceTo(startVec)) {
+            return entityResult;
+        } else {
+            return blockResult;
         }
     }
 
