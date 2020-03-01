3ad261834905c15686efd0c198f40d80dae19037
Lucas <lucasbandermann@gmail.com>
2020-03-01 13:57:43 +0100

    Fixed weird TESR package name

--- src/main/java/net/threetag/threecore/client/render/tileentity/FluidComposerTileEntityRenderer.java
+++ src/main/java/net/threetag/threecore/client/render/tileentity/FluidComposerTileEntityRenderer.java
@@ -1,20 +1,20 @@
-package net.threetag.threecore.client.render.tileentity.tileentity;
+package net.threetag.threecore.client.render.tileentity;
 
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.threetag.threecore.tileentity.StirlingGeneratorTileEntity;
+import net.threetag.threecore.tileentity.FluidComposerTileEntity;
 
 import java.util.Arrays;
 import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
-public class StirlingGeneratorTileEntityRenderer extends FastFluidTESR<StirlingGeneratorTileEntity> {
+public class FluidComposerTileEntityRenderer extends FastFluidTESR<FluidComposerTileEntity> {
 
     @Override
-    public List<TankRenderInfo> getTanksToRender(StirlingGeneratorTileEntity te) {
-        return Arrays.asList(new TankRenderInfo(te.fluidTank,
+    public List<FastFluidTESR.TankRenderInfo> getTanksToRender(FluidComposerTileEntity te) {
+        return Arrays.asList(new TankRenderInfo(te.outputFluidTank,
                 new AxisAlignedBB(2F / 16F, 8F / 16F, 2F / 16F, 14F / 16F, 15F / 16F, 14F / 16F),
                 Direction.UP));
     }
--- src/main/java/net/threetag/threecore/client/render/tileentity/StirlingGeneratorTileEntityRenderer.java
+++ src/main/java/net/threetag/threecore/client/render/tileentity/StirlingGeneratorTileEntityRenderer.java
@@ -1,20 +1,20 @@
-package net.threetag.threecore.client.render.tileentity.tileentity;
+package net.threetag.threecore.client.render.tileentity;
 
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.threetag.threecore.tileentity.FluidComposerTileEntity;
+import net.threetag.threecore.tileentity.StirlingGeneratorTileEntity;
 
 import java.util.Arrays;
 import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
-public class FluidComposerTileEntityRenderer extends FastFluidTESR<FluidComposerTileEntity> {
+public class StirlingGeneratorTileEntityRenderer extends FastFluidTESR<StirlingGeneratorTileEntity> {
 
     @Override
-    public List<FastFluidTESR.TankRenderInfo> getTanksToRender(FluidComposerTileEntity te) {
-        return Arrays.asList(new TankRenderInfo(te.outputFluidTank,
+    public List<TankRenderInfo> getTanksToRender(StirlingGeneratorTileEntity te) {
+        return Arrays.asList(new TankRenderInfo(te.fluidTank,
                 new AxisAlignedBB(2F / 16F, 8F / 16F, 2F / 16F, 14F / 16F, 15F / 16F, 14F / 16F),
                 Direction.UP));
     }
