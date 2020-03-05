789b63b231e23ce6900a96283f61db9e89bdcfe9
Lucas <lucasbandermann@gmail.com>
2020-03-05 18:06:39 +0100

    Decreased the amount of data ThreeData will need to be serialized

--- src/main/java/net/threetag/threecore/util/threedata/ThreeDataEntry.java
+++ src/main/java/net/threetag/threecore/util/threedata/ThreeDataEntry.java
@@ -1,29 +1,47 @@
 package net.threetag.threecore.util.threedata;
 
+import javax.annotation.Nonnull;
+
 public class ThreeDataEntry<T> {
 
     private final ThreeData<T> key;
     private T value;
+    private T defaultValue;
 
-    public ThreeDataEntry(ThreeData<T> key, T value) {
+    public ThreeDataEntry(@Nonnull ThreeData<T> key, @Nonnull T value, @Nonnull T defaultValue) {
         this.key = key;
         this.value = value;
+        this.defaultValue = defaultValue;
     }
 
     public ThreeData<T> getKey() {
         return this.key;
     }
 
-    public void setValue(T valueIn) {
-        this.value = valueIn;
+    public void setValue(@Nonnull T valueIn) {
+        if (valueIn.equals(defaultValue)) {
+            this.value = null;
+        } else {
+            this.value = valueIn;
+        }
     }
 
+    @Nonnull
     public T getValue() {
-        return this.value;
+        return this.value == null ? this.getDefaultValue() : this.value;
+    }
+
+    @Nonnull
+    public T getDefaultValue() {
+        return this.defaultValue;
+    }
+
+    public boolean holdsDefaultValue() {
+        return this.value == null;
     }
 
     public ThreeDataEntry<T> copy() {
-        return new ThreeDataEntry<T>(this.key, this.value);
+        return new ThreeDataEntry<T>(this.key, this.value, this.defaultValue);
     }
 
 }
