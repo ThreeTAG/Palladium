f7bd999d31fab60b01332eb5c31fd8abfcd60897
Lucas <lucasbandermann@gmail.com>
2020-09-02 18:11:33 +0200

    Changed some tag-related stuff

--- src/main/java/net/threetag/threecore/tags/TCEntityTypeTags.java
+++ src/main/java/net/threetag/threecore/tags/TCEntityTypeTags.java
@@ -2,19 +2,20 @@ package net.threetag.threecore.tags;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.tags.ITag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 import net.threetag.threecore.ThreeCore;
 
 public class TCEntityTypeTags {
 
-    public static final ITag.INamedTag<EntityType<?>> ARMOR_STANDS = make(ThreeCore.MODID, "armor_stands");
+    public static final Tags.IOptionalNamedTag<EntityType<?>> ARMOR_STANDS = make(ThreeCore.MODID, "armor_stands");
 
-    public static ITag.INamedTag<EntityType<?>> make(String name) {
-        return EntityTypeTags.func_232896_a_(ThreeCore.MODID + ":" + name);
+    public static Tags.IOptionalNamedTag<EntityType<?>> make(String name) {
+        return make(ThreeCore.MODID, name);
     }
 
-    public static ITag.INamedTag<EntityType<?>> make(String domain, String path) {
-        return EntityTypeTags.func_232896_a_(domain + ":" + path);
+    public static Tags.IOptionalNamedTag<EntityType<?>> make(String domain, String path) {
+        return EntityTypeTags.createOptional(new ResourceLocation(domain, path));
     }
 
 }
