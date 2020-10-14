e3d958d655cae26818227114fb79d264f4a9f00c
Lucas <lucasbandermann@gmail.com>
2020-10-14 17:36:04 +0200

    Added isSprinting method

--- src/main/java/net/threetag/threecore/scripts/accessors/LivingEntityAccessor.java
+++ src/main/java/net/threetag/threecore/scripts/accessors/LivingEntityAccessor.java
@@ -19,247 +19,216 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LivingEntityAccessor extends EntityAccessor
-{
+public class LivingEntityAccessor extends EntityAccessor {
 
-	public final LivingEntity livingEntity;
+    public final LivingEntity livingEntity;
 
-	protected LivingEntityAccessor(LivingEntity entity)
-	{
-		super(entity);
-		this.livingEntity = entity;
-	}
+    protected LivingEntityAccessor(LivingEntity entity) {
+        super(entity);
+        this.livingEntity = entity;
+    }
 
-	public boolean isChild()
-	{
-		return this.livingEntity.isChild();
-	}
+    public boolean isChild() {
+        return this.livingEntity.isChild();
+    }
 
-	public float getHealth()
-	{
-		return this.livingEntity.getHealth();
-	}
+    public float getHealth() {
+        return this.livingEntity.getHealth();
+    }
 
-	public void setHealth(@ScriptParameterName("health") float health)
-	{
-		this.livingEntity.setHealth(health);
-	}
+    public void setHealth(@ScriptParameterName("health") float health) {
+        this.livingEntity.setHealth(health);
+    }
 
-	public float getMaxHealth()
-	{
-		return this.livingEntity.getMaxHealth();
-	}
+    public float getMaxHealth() {
+        return this.livingEntity.getMaxHealth();
+    }
 
-	public void heal(@ScriptParameterName("amount") float amount)
-	{
-		this.livingEntity.heal(amount);
-	}
+    public void heal(@ScriptParameterName("amount") float amount) {
+        this.livingEntity.heal(amount);
+    }
 
-	public boolean isUndead()
-	{
-		return this.livingEntity.isEntityUndead();
-	}
+    public boolean isUndead() {
+        return this.livingEntity.isEntityUndead();
+    }
 
-	public boolean isOnLadder()
-	{
-		return this.livingEntity.isOnLadder();
-	}
+    public boolean isOnLadder() {
+        return this.livingEntity.isOnLadder();
+    }
 
-	public boolean isSleeping()
-	{
-		return this.livingEntity.isSleeping();
-	}
+    public boolean isSleeping() {
+        return this.livingEntity.isSleeping();
+    }
 
-	public boolean isElytraFlying()
-	{
-		return this.livingEntity.isElytraFlying();
-	}
+    public boolean isElytraFlying() {
+        return this.livingEntity.isElytraFlying();
+    }
 
-	public float getAbsorptionAmount()
-	{
-		return this.livingEntity.getAbsorptionAmount();
-	}
+    public float getAbsorptionAmount() {
+        return this.livingEntity.getAbsorptionAmount();
+    }
 
-	public float getMovementSpeed()
-	{
-		return this.livingEntity.getAIMoveSpeed();
-	}
+    public float getMovementSpeed() {
+        return this.livingEntity.getAIMoveSpeed();
+    }
 
-	public void setMovementSpeed(@ScriptParameterName("speed") float speed)
-	{
-		this.livingEntity.setAIMoveSpeed(speed);
-	}
+    public void setMovementSpeed(@ScriptParameterName("speed") float speed) {
+        this.livingEntity.setAIMoveSpeed(speed);
+    }
 
-	public void swingArm(@ScriptParameterName("mainHand") boolean mainHand)
-	{
-		this.livingEntity.swing(mainHand ? Hand.MAIN_HAND : Hand.OFF_HAND, true);
-	}
+    public boolean isSprinting() {
+        return this.livingEntity.isSprinting();
+    }
 
-	public ItemStackAccessor getItemInSlot(@ScriptParameterName("slot") String slot)
-	{
-		try
-		{
-			EquipmentSlotType slotType = EquipmentSlotType.fromString(slot);
-			return new ItemStackAccessor(this.livingEntity.getItemStackFromSlot(slotType));
-		}
-		catch (Exception e)
-		{
-			return ItemStackAccessor.EMPTY;
-		}
-	}
+    public void swingArm(@ScriptParameterName("mainHand") boolean mainHand) {
+        this.livingEntity.swing(mainHand ? Hand.MAIN_HAND : Hand.OFF_HAND, true);
+    }
 
-	public void setItemInSlot(@ScriptParameterName("slot") String slot, @ScriptParameterName("item") Object item)
-	{
-		try
-		{
-			EquipmentSlotType slotType = EquipmentSlotType.fromString(slot);
-			ItemStackAccessor stack = item instanceof ItemStackAccessor ? (ItemStackAccessor) item :
-					new ItemStackAccessor(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(item.toString()))));
-			this.livingEntity.setItemStackToSlot(slotType, stack.value);
-		}
-		catch (Exception e)
-		{
+    public ItemStackAccessor getItemInSlot(@ScriptParameterName("slot") String slot) {
+        try {
+            EquipmentSlotType slotType = EquipmentSlotType.fromString(slot);
+            return new ItemStackAccessor(this.livingEntity.getItemStackFromSlot(slotType));
+        } catch (Exception e) {
+            return ItemStackAccessor.EMPTY;
+        }
+    }
 
-		}
-	}
+    public void setItemInSlot(@ScriptParameterName("slot") String slot, @ScriptParameterName("item") Object item) {
+        try {
+            EquipmentSlotType slotType = EquipmentSlotType.fromString(slot);
+            ItemStackAccessor stack = item instanceof ItemStackAccessor ? (ItemStackAccessor) item :
+                    new ItemStackAccessor(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(item.toString()))));
+            this.livingEntity.setItemStackToSlot(slotType, stack.value);
+        } catch (Exception e) {
 
-	public AbilityAccessor[] getAbilities()
-	{
-		List<Ability> list = AbilityHelper.getAbilities(this.livingEntity);
-		AbilityAccessor[] array = new AbilityAccessor[list.size()];
+        }
+    }
 
-		for (int i = 0; i < list.size(); i++)
-		{
-			array[i] = new AbilityAccessor(list.get(i));
-		}
+    public AbilityAccessor[] getAbilities() {
+        List<Ability> list = AbilityHelper.getAbilities(this.livingEntity);
+        AbilityAccessor[] array = new AbilityAccessor[list.size()];
 
-		return array;
-	}
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = new AbilityAccessor(list.get(i));
+        }
 
-	public AbilityAccessor[] getAbilities(@ScriptParameterName("containerId") String containerId)
-	{
-		IAbilityContainer container = AbilityHelper.getAbilityContainerFromId(this.livingEntity, new ResourceLocation(containerId));
+        return array;
+    }
 
-		if (container != null)
-			return new AbilityAccessor[0];
+    public AbilityAccessor[] getAbilities(@ScriptParameterName("containerId") String containerId) {
+        IAbilityContainer container = AbilityHelper.getAbilityContainerFromId(this.livingEntity, new ResourceLocation(containerId));
 
-		Collection<AbilityAccessor> list = container.getAbilities().stream().map((a) -> new AbilityAccessor(a)).collect(Collectors.toList());
-		AbilityAccessor[] array = new AbilityAccessor[list.size()];
-		int i = 0;
+        if (container != null)
+            return new AbilityAccessor[0];
 
-		for (AbilityAccessor abilityAccessor : list)
-		{
-			array[i] = abilityAccessor;
-			i++;
-		}
+        Collection<AbilityAccessor> list = container.getAbilities().stream().map((a) -> new AbilityAccessor(a)).collect(Collectors.toList());
+        AbilityAccessor[] array = new AbilityAccessor[list.size()];
+        int i = 0;
 
-		return array;
-	}
+        for (AbilityAccessor abilityAccessor : list) {
+            array[i] = abilityAccessor;
+            i++;
+        }
 
-	public AbilityAccessor getAbilityById(@ScriptParameterName("abilityId") String id)
-	{
-		return (AbilityAccessor) AbilityAccessor.makeAccessor(AbilityHelper.getAbilityById(this.livingEntity, id, null));
-	}
+        return array;
+    }
 
-	public void attackEntityFrom(@ScriptParameterName("sourceName") String source, @ScriptParameterName("amount") float amount)
-	{
-		DamageSource damageSource = null;
-		switch (source)
-		{
-		case "inFire":
-			damageSource = DamageSource.IN_FIRE;
-			break;
-		case "lightningBolt":
-			damageSource = DamageSource.LIGHTNING_BOLT;
-			break;
-		case "onFire":
-			damageSource = DamageSource.ON_FIRE;
-			break;
-		case "lava":
-			damageSource = DamageSource.LAVA;
-			break;
-		case "hotFloor":
-			damageSource = DamageSource.HOT_FLOOR;
-			break;
-		case "inWall":
-			damageSource = DamageSource.IN_WALL;
-			break;
-		case "cramming":
-			damageSource = DamageSource.CRAMMING;
-			break;
-		case "drown":
-			damageSource = DamageSource.DROWN;
-			break;
-		case "starve":
-			damageSource = DamageSource.STARVE;
-			break;
-		case "cactus":
-			damageSource = DamageSource.CACTUS;
-			break;
-		case "fall":
-			damageSource = DamageSource.FALL;
-			break;
-		case "flyIntoWall":
-			damageSource = DamageSource.FLY_INTO_WALL;
-			break;
-		case "outOfWorld":
-			damageSource = DamageSource.OUT_OF_WORLD;
-			break;
-		case "generic":
-			damageSource = DamageSource.GENERIC;
-			break;
-		case "magic":
-			damageSource = DamageSource.MAGIC;
-			break;
-		case "wither":
-			damageSource = DamageSource.WITHER;
-			break;
-		case "anvil":
-			damageSource = DamageSource.ANVIL;
-			break;
-		case "fallingBlock":
-			damageSource = DamageSource.FALLING_BLOCK;
-			break;
-		case "dragonBreath":
-			damageSource = DamageSource.DRAGON_BREATH;
-			break;
-		case "dryout":
-			damageSource = DamageSource.DRYOUT;
-			break;
-		case "sweetBerryBush":
-			damageSource = DamageSource.SWEET_BERRY_BUSH;
-			break;
-		}
-		if (damageSource != null)
-			this.livingEntity.attackEntityFrom(damageSource, amount);
-	}
+    public AbilityAccessor getAbilityById(@ScriptParameterName("abilityId") String id) {
+        return (AbilityAccessor) AbilityAccessor.makeAccessor(AbilityHelper.getAbilityById(this.livingEntity, id, null));
+    }
 
-	public void attackEntityFromMob(@ScriptParameterName("attacker") LivingEntityAccessor entity, @ScriptParameterName("amount") float amount)
-	{
-		this.livingEntity.attackEntityFrom(DamageSource.causeMobDamage(entity.livingEntity), amount);
-	}
+    public void attackEntityFrom(@ScriptParameterName("sourceName") String source, @ScriptParameterName("amount") float amount) {
+        DamageSource damageSource = null;
+        switch (source) {
+            case "inFire":
+                damageSource = DamageSource.IN_FIRE;
+                break;
+            case "lightningBolt":
+                damageSource = DamageSource.LIGHTNING_BOLT;
+                break;
+            case "onFire":
+                damageSource = DamageSource.ON_FIRE;
+                break;
+            case "lava":
+                damageSource = DamageSource.LAVA;
+                break;
+            case "hotFloor":
+                damageSource = DamageSource.HOT_FLOOR;
+                break;
+            case "inWall":
+                damageSource = DamageSource.IN_WALL;
+                break;
+            case "cramming":
+                damageSource = DamageSource.CRAMMING;
+                break;
+            case "drown":
+                damageSource = DamageSource.DROWN;
+                break;
+            case "starve":
+                damageSource = DamageSource.STARVE;
+                break;
+            case "cactus":
+                damageSource = DamageSource.CACTUS;
+                break;
+            case "fall":
+                damageSource = DamageSource.FALL;
+                break;
+            case "flyIntoWall":
+                damageSource = DamageSource.FLY_INTO_WALL;
+                break;
+            case "outOfWorld":
+                damageSource = DamageSource.OUT_OF_WORLD;
+                break;
+            case "generic":
+                damageSource = DamageSource.GENERIC;
+                break;
+            case "magic":
+                damageSource = DamageSource.MAGIC;
+                break;
+            case "wither":
+                damageSource = DamageSource.WITHER;
+                break;
+            case "anvil":
+                damageSource = DamageSource.ANVIL;
+                break;
+            case "fallingBlock":
+                damageSource = DamageSource.FALLING_BLOCK;
+                break;
+            case "dragonBreath":
+                damageSource = DamageSource.DRAGON_BREATH;
+                break;
+            case "dryout":
+                damageSource = DamageSource.DRYOUT;
+                break;
+            case "sweetBerryBush":
+                damageSource = DamageSource.SWEET_BERRY_BUSH;
+                break;
+        }
+        if (damageSource != null)
+            this.livingEntity.attackEntityFrom(damageSource, amount);
+    }
 
-	public void attackEntityFromPlayer(@ScriptParameterName("playerAttacker") LivingEntityAccessor entity, @ScriptParameterName("amount") float amount)
-	{
-		if (entity.livingEntity instanceof PlayerEntity)
-			this.livingEntity.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) entity.livingEntity), amount);
-	}
+    public void attackEntityFromMob(@ScriptParameterName("attacker") LivingEntityAccessor entity, @ScriptParameterName("amount") float amount) {
+        this.livingEntity.attackEntityFrom(DamageSource.causeMobDamage(entity.livingEntity), amount);
+    }
 
-	public void attackEntityFromThrowable(@ScriptParameterName("throwable") EntityAccessor entity,
-			@ScriptParameterName("@Nullable thrower") EntityAccessor thrower, @ScriptParameterName("float") float amount)
-	{
-		this.livingEntity.attackEntityFrom(DamageSource.causeThrownDamage(entity.value, (thrower == null) ? null : thrower.value), amount);
-	}
+    public void attackEntityFromPlayer(@ScriptParameterName("playerAttacker") LivingEntityAccessor entity, @ScriptParameterName("amount") float amount) {
+        if (entity.livingEntity instanceof PlayerEntity)
+            this.livingEntity.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) entity.livingEntity), amount);
+    }
 
-	public void attackEntityFromExplosion(@ScriptParameterName("@Nullable attacker") LivingEntityAccessor attacker, @ScriptParameterName("float") float amount)
-	{
-		this.livingEntity.attackEntityFrom(DamageSource.causeExplosionDamage(attacker == null ? null : attacker.livingEntity), amount);
-	}
+    public void attackEntityFromThrowable(@ScriptParameterName("throwable") EntityAccessor entity,
+                                          @ScriptParameterName("@Nullable thrower") EntityAccessor thrower, @ScriptParameterName("float") float amount) {
+        this.livingEntity.attackEntityFrom(DamageSource.causeThrownDamage(entity.value, (thrower == null) ? null : thrower.value), amount);
+    }
 
-	public void addSuperpower(@ScriptParameterName("superpower") String superpower)
-	{
-		Superpower s = SuperpowerManager.getInstance().getSuperpower(new ResourceLocation(superpower));
-		if (s != null)
-			SuperpowerManager.addSuperpower(this.livingEntity, s);
-	}
+    public void attackEntityFromExplosion(@ScriptParameterName("@Nullable attacker") LivingEntityAccessor attacker, @ScriptParameterName("float") float amount) {
+        this.livingEntity.attackEntityFrom(DamageSource.causeExplosionDamage(attacker == null ? null : attacker.livingEntity), amount);
+    }
+
+    public void addSuperpower(@ScriptParameterName("superpower") String superpower) {
+        Superpower s = SuperpowerManager.getInstance().getSuperpower(new ResourceLocation(superpower));
+        if (s != null)
+            SuperpowerManager.addSuperpower(this.livingEntity, s);
+    }
 }
