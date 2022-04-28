8b09f3e90fa6b3a3800ad80d570439fffdfeb0a3
Lucas Bandermann <lucasbandermann@gmail.com>
2022-04-28 20:45:58 +0200

    Fixed projectile ability not assigning shooter correctly

--- common/src/main/java/net/threetag/palladium/power/ability/ProjectileAbility.java
+++ common/src/main/java/net/threetag/palladium/power/ability/ProjectileAbility.java
@@ -9,7 +9,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.entity.projectile.ThrowableProjectile;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.Vec3;
 import net.threetag.palladium.power.IPowerHolder;
@@ -42,8 +41,8 @@ public class ProjectileAbility extends Ability {
             compound.putString("id", Registry.ENTITY_TYPE.getKey(entry.getProperty(ENTITY_TYPE)).toString());
 
             ServerLevel world = (ServerLevel) entity.level;
-            EntityType.loadEntityRecursive(compound, world, (projectile) -> {
-                if (!(projectile instanceof Projectile))
+            EntityType.loadEntityRecursive(compound, world, (en) -> {
+                if (!(en instanceof Projectile projectile))
                     return null;
 
                 projectile.moveTo(entity.getX(), entity.getY() + entity.getEyeHeight() - 0.1D, entity.getZ(), projectile.getYRot(), projectile.getXRot());
@@ -53,13 +52,10 @@ public class ProjectileAbility extends Ability {
                 float f = -Mth.sin(entity.getYRot() * ((float) Math.PI / 180F)) * Mth.cos(entity.getXRot() * ((float) Math.PI / 180F));
                 float f1 = -Mth.sin((entity.getXRot() + pitchOffset) * ((float) Math.PI / 180F));
                 float f2 = Mth.cos(entity.getYRot() * ((float) Math.PI / 180F)) * Mth.cos(entity.getXRot() * ((float) Math.PI / 180F));
-                ((Projectile) projectile).shoot(f, f1, f2, velocity, inaccuracy);
+                projectile.shoot(f, f1, f2, velocity, inaccuracy);
                 Vec3 vec3d = entity.getDeltaMovement();
                 projectile.setDeltaMovement(projectile.getDeltaMovement().add(vec3d.x, entity.isOnGround() ? 0.0D : vec3d.y, vec3d.z));
-
-                if (projectile instanceof ThrowableProjectile) {
-                    ((ThrowableProjectile) projectile).setOwner(entity);
-                }
+                projectile.setOwner(entity);
 
                 if (entry.getProperty(THROW_SOUND) != null) {
                     PlayerUtil.playSoundToAll(entity.level, entity.getX(), entity.getY() + entity.getBbHeight() / 2D, entity.getZ(), 50, entry.getProperty(THROW_SOUND), entity.getSoundSource());
