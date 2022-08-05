4f9a5bf50acc440d545496e8165cc53b35209fe5
Lucas Bandermann <lucasbandermann@gmail.com>
2022-08-05 20:49:06 +0200

    Added ExtendedLangProvider

--- forge/src/main/java/net/threetag/palladium/data/forge/PalladiumLangProvider.java
+++ forge/src/main/java/net/threetag/palladium/data/forge/PalladiumLangProvider.java
@@ -1,22 +1,15 @@
 package net.threetag.palladium.data.forge;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.data.LanguageProvider;
 import net.threetag.palladium.Palladium;
 import net.threetag.palladium.accessory.Accessories;
-import net.threetag.palladium.accessory.Accessory;
 import net.threetag.palladium.block.PalladiumBlocks;
 import net.threetag.palladium.client.PalladiumKeyMappings;
 import net.threetag.palladium.item.PalladiumItems;
 import net.threetag.palladium.power.ability.Abilities;
-import net.threetag.palladium.power.ability.Ability;
 import net.threetag.palladium.sound.PalladiumSoundEvents;
 
-import java.util.Objects;
-import java.util.function.Supplier;
-
-public abstract class PalladiumLangProvider extends LanguageProvider {
+public abstract class PalladiumLangProvider extends ExtendedLangProvider {
 
     public PalladiumLangProvider(DataGenerator gen, String modid, String locale) {
         super(gen, modid, locale);
@@ -387,24 +380,6 @@ public abstract class PalladiumLangProvider extends LanguageProvider {
             // Subtitles
             this.add(PalladiumSoundDefinitionsProvider.subtitle(PalladiumSoundEvents.HEAT_VISION), "Hitzeblick");
         }
-    }
-
-    public void addAbility(Supplier<? extends Ability> key, String name) {
-        add(key.get(), name);
-    }
-
-    public void add(Ability key, String name) {
-        ResourceLocation id = Ability.REGISTRY.getId(key);
-        add("ability." + Objects.requireNonNull(id).getNamespace() + "." + id.getPath(), name);
-    }
-
-    protected void addAccessory(Supplier<? extends Accessory> key, String name) {
-        add(key.get(), name);
-    }
-
-    public void add(Accessory key, String name) {
-        ResourceLocation id = Accessory.REGISTRY.getId(key);
-        add("accessory." + Objects.requireNonNull(id).getNamespace() + "." + id.getPath(), name);
     }
 
     @Override
