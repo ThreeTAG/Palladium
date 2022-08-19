7e18b65df45fdfee0d79cf4c2d252053cb0237c4
Lucas Bandermann <lucasbandermann@gmail.com>
2022-08-19 15:31:06 +0200

    More 1.19 updating, should work now

--- common/src/main/java/net/threetag/palladium/mixin/VibrationListenerMixin.java
+++ common/src/main/java/net/threetag/palladium/mixin/VibrationListenerMixin.java
@@ -1,5 +1,6 @@
 package net.threetag.palladium.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -10,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(VibrationListener.VibrationListenerConfig.class)
-public interface VibrationListenerMixin {
+@Mixin(VibrationListener.class)
+public class VibrationListenerMixin {
 
-    @Inject(at = @At("HEAD"), method = "isValidVibration", cancellable = true)
-    private void isValidVibration(GameEvent gameEvent, GameEvent.Context context, CallbackInfoReturnable<Boolean> info) {
-        if (context.sourceEntity() instanceof LivingEntity livingEntity && livingEntity.getItemBySlot(EquipmentSlot.FEET).is(PalladiumItemTags.VIBRATION_ABSORPTION_BOOTS)) {
-            if (gameEvent == GameEvent.HIT_GROUND || gameEvent == GameEvent.STEP) {
+    @Inject(at = @At("HEAD"), method = "handleGameEvent", cancellable = true)
+    private void isValidVibration(ServerLevel serverLevel, GameEvent.Message message, CallbackInfoReturnable<Boolean> info) {
+        if (message.context().sourceEntity() instanceof LivingEntity livingEntity && livingEntity.getItemBySlot(EquipmentSlot.FEET).is(PalladiumItemTags.VIBRATION_ABSORPTION_BOOTS)) {
+            if (message.gameEvent() == GameEvent.HIT_GROUND || message.gameEvent() == GameEvent.STEP) {
                 info.setReturnValue(false);
             }
         }
