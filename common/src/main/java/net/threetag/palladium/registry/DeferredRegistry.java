855119a2abcd78728e23c2da6b53c98a99849c60
Lucraft <lucasbandermann@gmail.com>
2022-08-30 15:17:35 +0200

    Updated custom DeferredRegistry

--- common/src/main/java/net/threetag/palladium/registry/DeferredRegistry.java
+++ common/src/main/java/net/threetag/palladium/registry/DeferredRegistry.java
@@ -4,6 +4,7 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 
+import java.util.Collection;
 import java.util.function.Supplier;
 
 public abstract class DeferredRegistry<T> {
@@ -11,6 +12,8 @@ public abstract class DeferredRegistry<T> {
     public abstract void register();
 
     public abstract <R extends T> RegistrySupplier<R> register(String id, Supplier<R> supplier);
+
+    public abstract Collection<RegistrySupplier<T>> getEntries();
 
     @ExpectPlatform
     public static <T> DeferredRegistry<T> create(String modid, ResourceKey<? extends Registry<T>> resourceKey) {
--- forge/src/main/java/net/threetag/palladium/registry/forge/DeferredRegistryImpl.java
+++ forge/src/main/java/net/threetag/palladium/registry/forge/DeferredRegistryImpl.java
@@ -7,6 +7,9 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.threetag.palladium.registry.DeferredRegistry;
 import net.threetag.palladium.registry.RegistrySupplier;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class DeferredRegistryImpl {
@@ -15,12 +18,15 @@ public class DeferredRegistryImpl {
         return new Impl<>(modid, resourceKey);
     }
 
+    @SuppressWarnings("unchecked")
     public static class Impl<T> extends DeferredRegistry<T> {
 
         private final DeferredRegister<T> register;
+        private final List<RegistrySupplier<T>> entries;
 
         public Impl(String modid, ResourceKey<? extends Registry<T>> resourceKey) {
             this.register = DeferredRegister.create(resourceKey, modid);
+            this.entries = new ArrayList<>();
         }
 
         @Override
@@ -28,10 +34,18 @@ public class DeferredRegistryImpl {
             this.register.register(FMLJavaModLoadingContext.get().getModEventBus());
         }
 
+
         @Override
         public <R extends T> RegistrySupplier<R> register(String id, Supplier<R> supplier) {
             var orig = this.register.register(id, supplier);
-            return new RegistrySupplier<>(orig.getId(), orig);
+            var registrySupplier = new RegistrySupplier<>(orig.getId(), orig);
+            this.entries.add((RegistrySupplier<T>) registrySupplier);
+            return registrySupplier;
+        }
+
+        @Override
+        public Collection<RegistrySupplier<T>> getEntries() {
+            return this.entries;
         }
     }
 
