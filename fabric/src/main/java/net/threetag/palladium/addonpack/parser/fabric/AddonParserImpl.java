d8d3e17a79cc3fc41733c7009b41b9273862fe25
Lucraft <lucasbandermann@gmail.com>
2022-09-18 23:36:55 +0200

    Whoops

--- fabric/src/main/java/net/threetag/palladium/addonpack/parser/fabric/AddonParserImpl.java
+++ fabric/src/main/java/net/threetag/palladium/addonpack/parser/fabric/AddonParserImpl.java
@@ -4,19 +4,12 @@ import com.google.common.base.MoreObjects;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.threetag.palladium.addonpack.builder.AddonBuilder;
 
 public class AddonParserImpl {
 
     @SuppressWarnings({"unchecked", "ConstantConditions"})
     public static <T> void register(ResourceKey<? extends Registry<T>> key, AddonBuilder<T> builder) {
-        System.out.println("HALLO " + key.location().toString());
-
-        for (ResourceLocation objects : Registry.REGISTRY.keySet()) {
-            System.out.println(" - " + objects.toString());
-        }
-
         var registry = (Registry<T>) MoreObjects.firstNonNull(Registry.REGISTRY.get(key.location()), BuiltinRegistries.REGISTRY.get(key.location()));
         Registry.register(registry, builder.getId(), builder.get());
     }
