35825c02d42f3f97901b33e9ba574cf89c046864
Lucraft <lucasbandermann@gmail.com>
2022-10-01 21:16:37 +0200

    Added entity_type & entity_type_tag condition

---
+++ common/src/main/java/net/threetag/palladium/condition/EntityTypeCondition.java
@@ -0,0 +1,45 @@
+package net.threetag.palladium.condition;
+
+import com.google.gson.JsonObject;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.threetag.palladium.power.IPowerHolder;
+import net.threetag.palladium.power.Power;
+import net.threetag.palladium.power.ability.AbilityEntry;
+import net.threetag.palladium.util.property.EntityTypeProperty;
+import net.threetag.palladium.util.property.PalladiumProperty;
+
+public class EntityTypeCondition extends Condition {
+
+    private final EntityType<?> entityType;
+
+    public EntityTypeCondition(EntityType<?> entityType) {
+        this.entityType = entityType;
+    }
+
+    @Override
+    public boolean active(LivingEntity entity, AbilityEntry entry, Power power, IPowerHolder holder) {
+        return entity.getType() == this.entityType;
+    }
+
+    @Override
+    public ConditionSerializer getSerializer() {
+        return ConditionSerializers.ENTITY_TYPE.get();
+    }
+
+    public static class Serializer extends ConditionSerializer {
+
+        public static final PalladiumProperty<EntityType<?>> ENTITY_TYPE = new EntityTypeProperty("entity_type").configurable("The entity type the entity must be of for the condition to be active");
+
+        public Serializer() {
+            this.withProperty(ENTITY_TYPE, EntityType.PLAYER);
+        }
+
+        @Override
+        public Condition make(JsonObject json) {
+            return new EntityTypeCondition(this.getProperty(json, ENTITY_TYPE));
+        }
+
+    }
+
+}
---
+++ common/src/main/java/net/threetag/palladium/condition/EntityTypeTagCondition.java
@@ -0,0 +1,49 @@
+package net.threetag.palladium.condition;
+
+import com.google.gson.JsonObject;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.threetag.palladium.power.IPowerHolder;
+import net.threetag.palladium.power.Power;
+import net.threetag.palladium.power.ability.AbilityEntry;
+import net.threetag.palladium.util.property.PalladiumProperty;
+import net.threetag.palladium.util.property.ResourceLocationProperty;
+
+public class EntityTypeTagCondition extends Condition {
+
+    private final TagKey<EntityType<?>> tag;
+
+    public EntityTypeTagCondition(TagKey<EntityType<?>> tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public boolean active(LivingEntity entity, AbilityEntry entry, Power power, IPowerHolder holder) {
+        return entity.getType().is(this.tag);
+    }
+
+    @Override
+    public ConditionSerializer getSerializer() {
+        return ConditionSerializers.ENTITY_TYPE_TAG.get();
+    }
+
+    public static class Serializer extends ConditionSerializer {
+
+        public static final PalladiumProperty<ResourceLocation> ENTITY_TYPE = new ResourceLocationProperty("entity_type_tag").configurable("The tag the type of the entity must be in for the condition to be active");
+
+        public Serializer() {
+            this.withProperty(ENTITY_TYPE, EntityTypeTags.SKELETONS.location());
+        }
+
+        @Override
+        public Condition make(JsonObject json) {
+            return new EntityTypeTagCondition(TagKey.create(Registry.ENTITY_TYPE_REGISTRY, this.getProperty(json, ENTITY_TYPE)));
+        }
+
+    }
+
+}
