a300ed554e2e9cdb23926911d706a0dbdca6f31c
Lucraft <lucasbandermann@gmail.com>
2022-10-25 13:56:18 +0200

    Fixed crash with missing render layers

--- common/src/main/java/net/threetag/palladium/entity/BodyPart.java
+++ common/src/main/java/net/threetag/palladium/entity/BodyPart.java
@@ -167,9 +167,11 @@ public enum BodyPart {
         for (AbilityEntry entry : Ability.getEnabledEntries(entity, Abilities.RENDER_LAYER.get())) {
             IPackRenderLayer layer = PackRenderLayerManager.getInstance().getLayer(entry.getProperty(RenderLayerAbility.RENDER_LAYER));
 
-            for (BodyPart part : layer.getHiddenBodyParts(entity)) {
-                if (!bodyParts.contains(part)) {
-                    bodyParts.add(part);
+            if(layer != null) {
+                for (BodyPart part : layer.getHiddenBodyParts(entity)) {
+                    if (!bodyParts.contains(part)) {
+                        bodyParts.add(part);
+                    }
                 }
             }
         }
