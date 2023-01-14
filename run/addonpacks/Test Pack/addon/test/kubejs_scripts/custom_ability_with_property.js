2204aa718d23678c380f6dc04c680e5c7877ca12
Spyeedy <spyeedy8@gmail.com>
2023-01-14 20:35:49 +0800

    Cleaned up the custom_ability_with_property script

--- run/addonpacks/Test Pack/addon/test/kubejs_scripts/custom_ability_with_property.js
+++ run/addonpacks/Test Pack/addon/test/kubejs_scripts/custom_ability_with_property.js
@@ -1,14 +1,14 @@
 
     StartupEvents.registry("palladium:abilities", event => {
         event.create("testpack/test_ability_custom_prop")
-            .addProperty("selected_alien", "integer", 0, null)
+            .addProperty("some_important_number", "integer", 0, null)
             .firstTick((entity, entry, holder, enabled) => {
-                console.log("old alien:" + entry.getProperty("selected_alien"))
+                // console.log prints out to the log text files
+                console.log("my custom property:" + entry.getProperty("some_important_number"))
 
-                let new_alien = entry.getProperty("selected_alien") + 1;
-                let result = entry.setProperty("selected_alien", new_alien)
-                console.log("set alien result:" + result)
-
-                console.log("new alien:" + entry.getProperty("selected_alien"))
+                // note that you cannot set property, as the property just added can never be changed.
+                let new_alien = entry.getProperty("some_important_number") + 1;
+                let result = entry.setProperty("some_important_number", new_alien)
+                console.log("setProperty result:" + result) // result always prints out as false, because the property CANNOT be changed
             })
     })
