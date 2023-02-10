70cd19133cb31530b6aab70a4b30c7520fb56a30
Lucraft <lucasbandermann@gmail.com>
2023-02-10 21:21:20 +0100

    Fixed jump power attribute

--- common/src/main/java/net/threetag/palladium/mixin/LivingEntityMixin.java
+++ common/src/main/java/net/threetag/palladium/mixin/LivingEntityMixin.java
@@ -14,11 +14,12 @@ import java.util.Objects;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    @Shadow public abstract AttributeMap getAttributes();
+    @Shadow
+    public abstract AttributeMap getAttributes();
 
-    @Inject(method = "getJumpPower", at = @At("RETURN"))
+    @Inject(method = "getJumpPower", at = @At("RETURN"), cancellable = true)
     protected void getJumpPower(CallbackInfoReturnable<Float> cir) {
-        if(this.getAttributes().hasAttribute(PalladiumAttributes.JUMP_POWER.get())) {
+        if (this.getAttributes().hasAttribute(PalladiumAttributes.JUMP_POWER.get())) {
             var instance = this.getAttributes().getInstance(PalladiumAttributes.JUMP_POWER.get());
             Objects.requireNonNull(instance).setBaseValue(cir.getReturnValue());
             cir.setReturnValue((float) instance.getValue());
