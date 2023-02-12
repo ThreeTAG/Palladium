94a32478c7968e8c36ae628bc86539e5e72aab9b
Lucraft <lucasbandermann@gmail.com>
2023-02-12 16:59:56 +0100

    Made damage immunity properly cancel out damage

--- common/src/main/java/net/threetag/palladium/power/ability/AbilityEventHandler.java
+++ common/src/main/java/net/threetag/palladium/power/ability/AbilityEventHandler.java
@@ -5,20 +5,26 @@ import net.minecraft.world.entity.LivingEntity;
 import net.threetag.palladiumcore.event.EventResult;
 import net.threetag.palladiumcore.event.LivingEntityEvents;
 
-public class AbilityEventHandler implements LivingEntityEvents.Hurt {
+public class AbilityEventHandler implements LivingEntityEvents.Hurt, LivingEntityEvents.Attack {
 
     public static void init() {
         AbilityEventHandler handler = new AbilityEventHandler();
+        LivingEntityEvents.ATTACK.register(handler);
         LivingEntityEvents.HURT.register(handler);
     }
 
     @Override
     public EventResult livingEntityHurt(LivingEntity entity, DamageSource damageSource, float amount) {
-        for(AbilityEntry entry : AbilityUtil.getEnabledEntries(entity, Abilities.DAMAGE_IMMUNITY.get())) {
-            if(DamageImmunityAbility.isImmuneAgainst(entry, damageSource)) {
+        for (AbilityEntry entry : AbilityUtil.getEnabledEntries(entity, Abilities.DAMAGE_IMMUNITY.get())) {
+            if (DamageImmunityAbility.isImmuneAgainst(entry, damageSource)) {
                 return EventResult.cancel();
             }
         }
         return EventResult.pass();
+    }
+
+    @Override
+    public EventResult livingEntityAttack(LivingEntity entity, DamageSource damageSource, float amount) {
+        return this.livingEntityHurt(entity, damageSource, amount);
     }
 }
