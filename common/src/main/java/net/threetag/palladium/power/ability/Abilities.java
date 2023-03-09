42c084d97bcc0586e4566e7ab221cbebbf0fb426
Lucraft <lucasbandermann@gmail.com>
2023-03-09 21:30:08 +0100

    Added missing documentation descriptions for abilities

--- common/src/main/java/net/threetag/palladium/power/ability/Abilities.java
+++ common/src/main/java/net/threetag/palladium/power/ability/Abilities.java
@@ -10,7 +10,7 @@ public class Abilities {
 
     public static final DeferredRegister<Ability> ABILITIES = DeferredRegister.create(Palladium.MOD_ID, Ability.REGISTRY);
 
-    public static final RegistrySupplier<Ability> DUMMY = ABILITIES.register("dummy", Ability::new);
+    public static final RegistrySupplier<Ability> DUMMY = ABILITIES.register("dummy", () -> new Ability().setDocumentationDescription("Does nothing."));
     public static final RegistrySupplier<Ability> COMMAND = ABILITIES.register("command", CommandAbility::new);
     public static final RegistrySupplier<Ability> RENDER_LAYER = ABILITIES.register("render_layer", RenderLayerAbility::new);
     public static final RegistrySupplier<Ability> ANIMATION_TIMER = ABILITIES.register("animation_timer", AnimationTimerAbility::new);
@@ -19,7 +19,7 @@ public class Abilities {
     public static final RegistrySupplier<Ability> HEALING = ABILITIES.register("healing", HealingAbility::new);
     public static final RegistrySupplier<Ability> SLOWFALL = ABILITIES.register("slowfall", SlowfallAbility::new);
     public static final RegistrySupplier<Ability> DAMAGE_IMMUNITY = ABILITIES.register("damage_immunity", DamageImmunityAbility::new);
-    public static final RegistrySupplier<Ability> INVISIBILITY = ABILITIES.register("invisibility", () -> new Ability().withProperty(Ability.ICON, new TexturedIcon(new ResourceLocation(Palladium.MOD_ID, "textures/icon/invisibility.png"))));
+    public static final RegistrySupplier<Ability> INVISIBILITY = ABILITIES.register("invisibility", () -> new Ability().withProperty(Ability.ICON, new TexturedIcon(new ResourceLocation(Palladium.MOD_ID, "textures/icon/invisibility.png"))).setDocumentationDescription("Makes the player invisible. Also makes mobs not see the player anymore."));
     public static final RegistrySupplier<Ability> ENERGY_BLAST = ABILITIES.register("energy_blast", EnergyBlastAbility::new);
     public static final RegistrySupplier<Ability> SIZE = ABILITIES.register("size", SizeAbility::new);
     public static final RegistrySupplier<Ability> PROJECTILE = ABILITIES.register("projectile", ProjectileAbility::new);
@@ -28,9 +28,9 @@ public class Abilities {
     public static final RegistrySupplier<Ability> HIDE_BODY_PARTS = ABILITIES.register("hide_body_parts", HideBodyPartsAbility::new);
     public static final RegistrySupplier<Ability> SHADER_EFFECT = ABILITIES.register("shader_effect", ShaderEffectAbility::new);
     public static final RegistrySupplier<Ability> GUI_OVERLAY = ABILITIES.register("gui_overlay", GuiOverlayAbility::new);
-    public static final RegistrySupplier<Ability> SHOW_BOTH_ARMS = ABILITIES.register("show_both_arms", () -> new Ability().withProperty(Ability.HIDDEN, true));
+    public static final RegistrySupplier<Ability> SHOW_BOTH_ARMS = ABILITIES.register("show_both_arms", () -> new Ability().withProperty(Ability.HIDDEN, true).setDocumentationDescription("Enables the rendering of your off-hand."));
     public static final RegistrySupplier<Ability> PLAYER_ANIMATION = ABILITIES.register("player_animation", PlayerAnimationAbility::new);
-    public static final RegistrySupplier<Ability> WATER_WALK = ABILITIES.register("water_walk", Ability::new);
+    public static final RegistrySupplier<Ability> WATER_WALK = ABILITIES.register("water_walk", () -> new Ability().setDocumentationDescription("Allows the player to walk on water."));
     public static final RegistrySupplier<Ability> RESTRICT_SLOTS = ABILITIES.register("restrict_slots", RestrictSlotsAbility::new);
     public static final RegistrySupplier<Ability> PLAY_SOUND = ABILITIES.register("play_sound", PlaySoundAbility::new);
 
