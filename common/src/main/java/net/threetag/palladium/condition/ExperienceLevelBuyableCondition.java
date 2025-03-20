02ffbcf4914dfd8ab2ed3f02e46768eb395bc60d
Lucas Bandermann <lucasbandermann@threetag.net>
2025-03-20 19:29:20 +0100

    Added "flat_levels" option for xp_level_buyable condition to switch between old and new logic

--- common/src/main/java/net/threetag/palladium/condition/ExperienceLevelBuyableCondition.java
+++ common/src/main/java/net/threetag/palladium/condition/ExperienceLevelBuyableCondition.java
@@ -6,15 +6,18 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.threetag.palladium.power.ability.AbilityConfiguration;
 import net.threetag.palladium.util.icon.ExperienceIcon;
+import net.threetag.palladium.util.property.BooleanProperty;
 import net.threetag.palladium.util.property.IntegerProperty;
 import net.threetag.palladium.util.property.PalladiumProperty;
 
 public class ExperienceLevelBuyableCondition extends BuyableCondition {
 
     private final int xpLevel;
+    private final boolean flatLevels;
 
-    public ExperienceLevelBuyableCondition(int xpLevel) {
+    public ExperienceLevelBuyableCondition(int xpLevel, boolean flatLevels) {
         this.xpLevel = xpLevel;
+        this.flatLevels = flatLevels;
     }
 
     @Override
@@ -34,40 +37,15 @@ public class ExperienceLevelBuyableCondition extends BuyableCondition {
     @Override
     public boolean takeFromEntity(LivingEntity entity) {
         if (entity instanceof Player player && this.isAvailable(entity)) {
-            player.giveExperiencePoints(-getTotalXpForLevel(this.xpLevel));
+            if (this.flatLevels) {
+                player.giveExperienceLevels(-this.xpLevel);
+            } else {
+                player.giveExperiencePoints(-getTotalXpForLevel(this.xpLevel));
+            }
             return true;
         }
 
         return false;
-    }
-
-    @Override
-    public ConditionSerializer getSerializer() {
-        return ConditionSerializers.EXPERIENCE_LEVEL_BUYABLE.get();
-    }
-
-    public static class Serializer extends ConditionSerializer {
-
-        public static final PalladiumProperty<Integer> XP_LEVEL = new IntegerProperty("xp_level").configurable("Amount xp level that the player needs to spend");
-
-        public Serializer() {
-            this.withProperty(XP_LEVEL, 3);
-        }
-
-        @Override
-        public ConditionEnvironment getContextEnvironment() {
-            return ConditionEnvironment.DATA;
-        }
-
-        @Override
-        public Condition make(JsonObject json) {
-            return new ExperienceLevelBuyableCondition(getProperty(json, XP_LEVEL));
-        }
-
-        @Override
-        public String getDocumentationDescription() {
-            return "A condition that makes the ability buyable for a certain amount of xp levels.";
-        }
     }
 
     private int getTotalXpForLevel(int level) {
@@ -80,4 +58,36 @@ public class ExperienceLevelBuyableCondition extends BuyableCondition {
             return (int) (4.5 * level * level - 162.5 * level + 2220);
         }
     }
+
+    @Override
+    public ConditionSerializer getSerializer() {
+        return ConditionSerializers.EXPERIENCE_LEVEL_BUYABLE.get();
+    }
+
+    public static class Serializer extends ConditionSerializer {
+
+        public static final PalladiumProperty<Integer> XP_LEVEL = new IntegerProperty("xp_level").configurable("Amount xp level that the player needs to spend");
+        public static final PalladiumProperty<Boolean> FLAT_LEVELS = new BooleanProperty("flat_levels").configurable("Determines if the taken levels should be taken as-is, or be calculated appropriately to Minecraft logic.");
+
+        public Serializer() {
+            this.withProperty(XP_LEVEL, 3);
+            this.withProperty(FLAT_LEVELS, true);
+        }
+
+        @Override
+        public ConditionEnvironment getContextEnvironment() {
+            return ConditionEnvironment.DATA;
+        }
+
+        @Override
+        public Condition make(JsonObject json) {
+            return new ExperienceLevelBuyableCondition(getProperty(json, XP_LEVEL), getProperty(json, FLAT_LEVELS));
+        }
+
+        @Override
+        public String getDocumentationDescription() {
+            return "A condition that makes the ability buyable for a certain amount of xp levels.";
+        }
+    }
+
 }
