e4a1ad0ab3093decf2f07ca8d3a976448bfe75c9
Lucas Bandermann <lucasbandermann@threetag.net>
2025-05-08 19:23:26 +0200

    Added palladium property filter for entity glow ability

--- common/src/main/java/net/threetag/palladium/power/ability/EntityGlowAbility.java
+++ common/src/main/java/net/threetag/palladium/power/ability/EntityGlowAbility.java
@@ -7,18 +7,23 @@ import net.threetag.palladium.util.icon.ItemIcon;
 import net.threetag.palladium.util.property.*;
 
 import java.awt.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EntityGlowAbility extends Ability {
 
     public static final PalladiumProperty<Mode> MODE = new EntityGlowModeProperty("mode").configurable("Determines if this ability makes the entity itself glow, or lets you see other entities with their glow effect.");
     public static final PalladiumProperty<Color> COLOR = new ColorProperty("color").configurable("Determines the color of the glow effect. If left empty, it will use the default team color.");
     public static final PalladiumProperty<Double> DISTANCE = new DoubleProperty("distance").configurable("The distance at which the glow effect is visible. If left empty, it will behave normally.");
+    public static final PalladiumProperty<String> PROPERTY_KEY = new StringProperty("property_key").configurable("If you want to make the glow dependent on a Palladium Property, this is the key/name of it which you can specify.");
+    public static final PalladiumProperty<String> PROPERTY_VALUE = new StringProperty("property_value").configurable("If you want to make the glow dependent on a Palladium Property, this is value of it which you can specify.");
 
     public EntityGlowAbility() {
         this.withProperty(ICON, new ItemIcon(Items.GLOW_BERRIES))
                 .withProperty(MODE, Mode.SELF)
                 .withProperty(COLOR, null)
-                .withProperty(DISTANCE, 0D);
+                .withProperty(DISTANCE, 0D)
+                .withProperty(PROPERTY_KEY, null)
+                .withProperty(PROPERTY_VALUE, null);
     }
 
     @Override
@@ -29,7 +34,7 @@ public class EntityGlowAbility extends Ability {
     public static boolean shouldGlow(Entity renderedEntity, Entity cameraEntity) {
         if (renderedEntity instanceof LivingEntity living) {
             for (AbilityInstance ability : AbilityUtil.getEnabledInstances(living, Abilities.ENTITY_GLOW.get())) {
-                if (ability.getProperty(EntityGlowAbility.MODE) == Mode.SELF) {
+                if (ability.getProperty(EntityGlowAbility.MODE) == Mode.SELF && propertyCheck(renderedEntity, ability.getProperty(PROPERTY_KEY), ability.getProperty(PROPERTY_VALUE))) {
                     double maxDistance = ability.getProperty(EntityGlowAbility.DISTANCE);
                     return maxDistance <= 0D || renderedEntity == cameraEntity || renderedEntity.distanceToSqr(cameraEntity) <= maxDistance * maxDistance;
                 }
@@ -38,7 +43,7 @@ public class EntityGlowAbility extends Ability {
 
         if (renderedEntity != cameraEntity && cameraEntity instanceof LivingEntity living) {
             for (AbilityInstance ability : AbilityUtil.getEnabledInstances(living, Abilities.ENTITY_GLOW.get())) {
-                if (ability.getProperty(EntityGlowAbility.MODE) == Mode.OTHERS) {
+                if (ability.getProperty(EntityGlowAbility.MODE) == Mode.OTHERS && propertyCheck(renderedEntity, ability.getProperty(PROPERTY_KEY), ability.getProperty(PROPERTY_VALUE))) {
                     double maxDistance = ability.getProperty(EntityGlowAbility.DISTANCE);
                     return maxDistance <= 0D || renderedEntity.distanceToSqr(cameraEntity) <= maxDistance * maxDistance;
                 }
@@ -46,6 +51,26 @@ public class EntityGlowAbility extends Ability {
         }
 
         return false;
+    }
+
+    private static boolean propertyCheck(Entity entity, String key, String value) {
+        if (key == null || value == null) {
+            return true;
+        }
+
+        var result = new AtomicBoolean(false);
+        EntityPropertyHandler.getHandler(entity).ifPresent(handler -> {
+            var prop = handler.getPropertyByName(key);
+
+            if (prop != null) {
+                Object val = handler.get(prop);
+
+                if (val != null) {
+                    result.set(val.toString().equals(value));
+                }
+            }
+        });
+        return result.get();
     }
 
     public enum Mode {
