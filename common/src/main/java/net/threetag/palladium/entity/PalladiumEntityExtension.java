d7a6fc88734eaa31f867c6abe1000ddd5394b422
Lucas Bandermann <lucasbandermann@threetag.net>
2025-05-15 18:45:20 +0200

    is_moving condition improvement

--- common/src/main/java/net/threetag/palladium/entity/PalladiumEntityExtension.java
+++ common/src/main/java/net/threetag/palladium/entity/PalladiumEntityExtension.java
@@ -1,5 +1,6 @@
 package net.threetag.palladium.entity;
 
+import net.minecraft.world.phys.Vec3;
 import net.threetag.palladium.util.property.EntityPropertyHandler;
 
 public interface PalladiumEntityExtension {
@@ -7,5 +8,7 @@ public interface PalladiumEntityExtension {
     EntityPropertyHandler palladium$getPropertyHandler();
 
     TrailHandler palladium$getTrailHandler();
+
+    Vec3 palladium$getLastTickPos();
 
 }
