d0ee35afa0662778b2840601bc1ebc8dee2871fd
Lucas Bandermann <lucasbandermann@gmail.com>
2025-06-15 20:48:07 +0200

    Added "visibility" field for energy beams

--- common/src/main/java/net/threetag/palladium/client/energybeam/LaserBeamRenderer.java
+++ common/src/main/java/net/threetag/palladium/client/energybeam/LaserBeamRenderer.java
@@ -1,5 +1,6 @@
 package net.threetag.palladium.client.energybeam;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -9,27 +10,32 @@ import net.minecraft.world.phys.Vec3;
 import net.threetag.palladium.Palladium;
 import net.threetag.palladium.client.renderer.LaserRenderer;
 import net.threetag.palladium.documentation.JsonDocumentationBuilder;
+import net.threetag.palladium.util.PerspectiveValue;
 import net.threetag.palladium.util.SizeUtil;
 import org.joml.Vector2f;
 
 public class LaserBeamRenderer extends EnergyBeamRenderer {
 
     private final LaserRenderer laserRenderer;
+    private final PerspectiveValue<Boolean> visibility;
 
-    public LaserBeamRenderer(LaserRenderer laserRenderer) {
+    public LaserBeamRenderer(LaserRenderer laserRenderer, PerspectiveValue<Boolean> visibility) {
         this.laserRenderer = laserRenderer;
+        this.visibility = visibility;
     }
 
     @Override
     public void render(AbstractClientPlayer player, Vec3 origin, Vec3 target, float lengthMultiplier, PoseStack poseStack, MultiBufferSource bufferSource, int packedLightIn, boolean isFirstPerson, float partialTick) {
-        var size = this.laserRenderer.getSize();
+        if (this.visibility.getForPlayer(player)) {
+            var size = this.laserRenderer.getSize();
 
-        this.laserRenderer
-                .size(size.mul(SizeUtil.getInstance().getModelWidthScale(player, partialTick), SizeUtil.getInstance().getModelHeightScale(player, partialTick), new Vector2f()))
-                .length((float) origin.distanceTo(target) * lengthMultiplier)
-                .faceAndRender(poseStack, bufferSource, origin, target, player.tickCount, partialTick);
+            this.laserRenderer
+                    .size(size.mul(SizeUtil.getInstance().getModelWidthScale(player, partialTick), SizeUtil.getInstance().getModelHeightScale(player, partialTick), new Vector2f()))
+                    .length((float) origin.distanceTo(target) * lengthMultiplier)
+                    .faceAndRender(poseStack, bufferSource, origin, target, player.tickCount, partialTick);
 
-        this.laserRenderer.size(size);
+            this.laserRenderer.size(size);
+        }
     }
 
     public static class Serializer extends EnergyBeamRenderer.Serializer {
@@ -38,13 +44,20 @@ public class LaserBeamRenderer extends EnergyBeamRenderer {
 
         @Override
         public EnergyBeamRenderer fromJson(JsonObject json) {
-            return new LaserBeamRenderer(LaserRenderer.fromJson(json));
+            return new LaserBeamRenderer(LaserRenderer.fromJson(json), PerspectiveValue.getFromJson(json, "visibility", JsonElement::getAsBoolean, true));
         }
 
         @Override
         public void generateDocumentation(JsonDocumentationBuilder builder) {
             builder.setTitle("Laser");
             LaserRenderer.generateDocumentation(builder, 2, false);
+
+            var exampleJson = new JsonObject();
+            exampleJson.addProperty("first_person", true);
+            exampleJson.addProperty("third_person", true);
+            builder.addProperty("visibility", Boolean.class)
+                    .description("Determines if its visible.")
+                    .fallback(true).exampleJson(exampleJson);
         }
 
         @Override
