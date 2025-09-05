81da340a71a3c1bafbdcaad5fa93fdc102e3cc07
Lucas Bandermann <lucasbandermann@threetag.net>
2025-09-05 22:26:14 +0200

    Added "tint" option to skin_overlay render layers

--- common/src/main/java/net/threetag/palladium/client/renderer/renderlayer/SkinOverlayPackRenderLayer.java
+++ common/src/main/java/net/threetag/palladium/client/renderer/renderlayer/SkinOverlayPackRenderLayer.java
@@ -6,6 +6,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
@@ -16,25 +17,45 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.HumanoidArm;
 import net.threetag.palladium.client.dynamictexture.DynamicTexture;
 import net.threetag.palladium.client.dynamictexture.DynamicTextureManager;
+import net.threetag.palladium.client.renderer.DynamicColor;
 import net.threetag.palladium.util.SkinTypedValue;
 import net.threetag.palladium.util.context.DataContext;
+
+import java.awt.*;
 
 public class SkinOverlayPackRenderLayer extends AbstractPackRenderLayer {
 
     private final SkinTypedValue<DynamicTexture> texture;
     private final RenderTypeFunction renderType;
+    private final DynamicColor tint;
 
-    public SkinOverlayPackRenderLayer(SkinTypedValue<DynamicTexture> texture, RenderTypeFunction renderType) {
+    public SkinOverlayPackRenderLayer(SkinTypedValue<DynamicTexture> texture, RenderTypeFunction renderType, DynamicColor tint) {
         this.texture = texture;
         this.renderType = renderType;
+        this.tint = tint;
     }
 
     @Override
     public void render(DataContext context, PoseStack poseStack, MultiBufferSource bufferSource, EntityModel<Entity> parentModel, int packedLight, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         var entity = context.getEntity();
         if (IPackRenderLayer.conditionsFulfilled(entity, this.conditions, this.thirdPersonConditions)) {
+            var tint = Color.WHITE;
+
+            if (this.tint != null) {
+                tint = this.tint.getColor(context);
+            }
+
             VertexConsumer vertexConsumer = this.renderType.createVertexConsumer(bufferSource, this.texture.get(entity).getTexture(context), context.getItem().hasFoil());
-            parentModel.renderToBuffer(poseStack, vertexConsumer, this.renderType.getPackedLight(packedLight), OverlayTexture.NO_OVERLAY, 1F, 1F, 1F, 1F);
+            parentModel.renderToBuffer(
+                    poseStack,
+                    vertexConsumer,
+                    this.renderType.getPackedLight(packedLight),
+                    OverlayTexture.NO_OVERLAY,
+                    tint.getRed() / 255F,
+                    tint.getGreen() / 255F,
+                    tint.getBlue() / 255F,
+                    tint.getAlpha() / 255F
+            );
         }
     }
 
@@ -49,13 +70,31 @@ public class SkinOverlayPackRenderLayer extends AbstractPackRenderLayer {
             entityModel.crouching = false;
             entityModel.swimAmount = 0.0F;
 
-            if (arm == HumanoidArm.RIGHT) {
-                entityModel.rightArm.xRot = 0.0F;
-                entityModel.rightArm.render(poseStack, vertexConsumer, this.renderType.getPackedLight(packedLight), OverlayTexture.NO_OVERLAY);
-            } else {
-                entityModel.leftArm.xRot = 0.0F;
-                entityModel.leftArm.render(poseStack, vertexConsumer, this.renderType.getPackedLight(packedLight), OverlayTexture.NO_OVERLAY);
+            var tint = Color.WHITE;
+
+            if (this.tint != null) {
+                tint = this.tint.getColor(context);
             }
+
+            ModelPart armToRender;
+
+            if (arm == HumanoidArm.RIGHT) {
+                armToRender = entityModel.rightArm;
+            } else {
+                armToRender = entityModel.leftArm;
+            }
+
+            armToRender.xRot = 0.0F;
+            armToRender.render(
+                    poseStack,
+                    vertexConsumer,
+                    this.renderType.getPackedLight(packedLight),
+                    OverlayTexture.NO_OVERLAY,
+                    tint.getRed() / 255F,
+                    tint.getGreen() / 255F,
+                    tint.getBlue() / 255F,
+                    tint.getAlpha() / 255F
+            );
         }
     }
 
@@ -66,6 +105,10 @@ public class SkinOverlayPackRenderLayer extends AbstractPackRenderLayer {
             throw new JsonParseException("Unknown render type '" + new ResourceLocation(GsonHelper.getAsString(json, "render_type", "solid")) + "'");
         }
 
-        return new SkinOverlayPackRenderLayer(SkinTypedValue.fromJSON(json.get("texture"), DynamicTextureManager::fromJson), renderType);
+        return new SkinOverlayPackRenderLayer(
+                SkinTypedValue.fromJSON(json.get("texture"), DynamicTextureManager::fromJson),
+                renderType,
+                DynamicColor.getFromJson(json, "tint", null)
+        );
     }
 }
