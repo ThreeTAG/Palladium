a05ac5e44f97a8d1fab9b26f54e7b5dff2af193e
Lucas Bandermann <lucas@threetag.net>
2025-10-28 21:07:23 +0100

    Fixed watcher renderer

--- src/main/java/net/threetag/palladium/client/renderer/WatcherRenderer.java
+++ src/main/java/net/threetag/palladium/client/renderer/WatcherRenderer.java
@@ -1,7 +1,16 @@
 package net.threetag.palladium.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
+import com.mojang.blaze3d.buffers.GpuBuffer;
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import com.mojang.blaze3d.systems.RenderPass;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.textures.GpuTextureView;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.neoforged.api.distmarker.Dist;
@@ -10,6 +19,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.threetag.palladium.Palladium;
 import net.threetag.palladium.addonpack.log.AddonPackLog;
+import org.joml.Matrix4f;
+import org.joml.Matrix4fStack;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import javax.annotation.Nullable;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 
 @EventBusSubscriber(modid = Palladium.MOD_ID, value = Dist.CLIENT)
 public class WatcherRenderer {
@@ -21,35 +38,84 @@ public class WatcherRenderer {
     private static final int OCCURRENCE_DURATION = 20 * 60; // 1 minute
     private static final int OCCURRENCE_FADE = 40; // 2 seconds
 
+    @Nullable
+    private AbstractTexture texture;
+    private GpuBuffer buffer;
+    private RenderSystem.AutoStorageIndexBuffer quadIndices;
     private int ticksTilOccurrence = OCCURRENCE_INTERVAL;
     private int visibleTicks = 0;
     private int visibility = 0;
     private int prevVisibility = 0;
 
-    public WatcherRenderer() {
+    public void init() {
+        this.texture = this.getTexture(TEXTURE);
 
+        if (this.buffer == null) {
+            this.buffer = this.buildQuad();
+        }
+
+        if (this.quadIndices == null) {
+            this.quadIndices = RenderSystem.getSequentialBuffer(VertexFormat.Mode.QUADS);
+        }
     }
 
-    public void render(PoseStack poseStack, MultiBufferSource.BufferSource bufferSource, float visibility) {
-//        if (visibility > 0F) {
-//            var easing = this.visibleTicks > 0 ? Easing.INSINE : Easing.OUTSINE;
-//            visibility = easing.apply(visibility);
-//
-//            poseStack.pushPose();
-//            poseStack.mulPose(Axis.YP.rotationDegrees(-135.0F));
-//            poseStack.mulPose(Axis.XP.rotationDegrees(60));
-//            float f = 40.0F;
-//            float g = 100.0F;
-//            VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.celestial(TEXTURE));
-//            int i = ARGB.white(visibility);
-//            Matrix4f matrix4f = poseStack.last().pose();
-//            vertexConsumer.addVertex(matrix4f, -f, g, -f).setUv(1.0F, 0.0F).setColor(i);
-//            vertexConsumer.addVertex(matrix4f, f, g, -f).setUv(0.0F, 0.0F).setColor(i);
-//            vertexConsumer.addVertex(matrix4f, f, g, f).setUv(0.0F, 1.0F).setColor(i);
-//            vertexConsumer.addVertex(matrix4f, -f, g, f).setUv(1.0F, 1.0F).setColor(i);
-//            bufferSource.endBatch();
-//            poseStack.popPose();
-//        }
+    private AbstractTexture getTexture(ResourceLocation location) {
+        TextureManager texturemanager = Minecraft.getInstance().getTextureManager();
+        AbstractTexture abstracttexture = texturemanager.getTexture(location);
+        abstracttexture.setUseMipmaps(false);
+        return abstracttexture;
+    }
+
+    public void render(PoseStack poseStack, float visibility) {
+        if (this.texture != null && visibility > 0F) {
+            poseStack.pushPose();
+            poseStack.mulPose(Axis.YP.rotationDegrees(-135.0F));
+            poseStack.mulPose(Axis.XP.rotationDegrees(60F));
+
+            Matrix4fStack matrix4fstack = RenderSystem.getModelViewStack();
+            matrix4fstack.pushMatrix();
+            matrix4fstack.mul(poseStack.last().pose());
+            matrix4fstack.translate(0.0F, 100.0F, 0.0F);
+            matrix4fstack.scale(40.0F, 1.0F, 40.0F);
+            GpuBufferSlice gpubufferslice = RenderSystem.getDynamicUniforms()
+                    .writeTransform(matrix4fstack, new Vector4f(1.0F, 1.0F, 1.0F, visibility), new Vector3f(), new Matrix4f(), 0.0F);
+            GpuTextureView gputextureview = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
+            GpuTextureView gputextureview1 = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
+            GpuBuffer gpubuffer = this.quadIndices.getBuffer(6);
+
+            try (RenderPass renderpass = RenderSystem.getDevice()
+                    .createCommandEncoder()
+                    .createRenderPass(() -> "Sky watcher", gputextureview, OptionalInt.empty(), gputextureview1, OptionalDouble.empty())) {
+                renderpass.setPipeline(RenderPipelines.CELESTIAL);
+                RenderSystem.bindDefaultUniforms(renderpass);
+                renderpass.setUniform("DynamicTransforms", gpubufferslice);
+                renderpass.bindSampler("Sampler0", this.texture.getTextureView());
+                renderpass.setVertexBuffer(0, this.buffer);
+                renderpass.setIndexBuffer(gpubuffer, this.quadIndices.type());
+                renderpass.drawIndexed(0, 0, 6, 1);
+            }
+
+            matrix4fstack.popMatrix();
+            poseStack.popPose();
+        }
+    }
+
+    private GpuBuffer buildQuad() {
+        GpuBuffer gpubuffer;
+        try (ByteBufferBuilder bytebufferbuilder = ByteBufferBuilder.exactlySized(4 * DefaultVertexFormat.POSITION_TEX.getVertexSize())) {
+            BufferBuilder bufferbuilder = new BufferBuilder(bytebufferbuilder, VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+            Matrix4f matrix4f = new Matrix4f();
+            bufferbuilder.addVertex(matrix4f, -1.0F, 0.0F, -1.0F).setUv(0.0F, 0.0F);
+            bufferbuilder.addVertex(matrix4f, 1.0F, 0.0F, -1.0F).setUv(1.0F, 0.0F);
+            bufferbuilder.addVertex(matrix4f, 1.0F, 0.0F, 1.0F).setUv(1.0F, 1.0F);
+            bufferbuilder.addVertex(matrix4f, -1.0F, 0.0F, 1.0F).setUv(0.0F, 1.0F);
+
+            try (MeshData meshdata = bufferbuilder.buildOrThrow()) {
+                gpubuffer = RenderSystem.getDevice().createBuffer(() -> "Sun quad", 40, meshdata.vertexBuffer());
+            }
+        }
+
+        return gpubuffer;
     }
 
     @SubscribeEvent
--- src/main/java/net/threetag/palladium/mixin/client/SkyRendererMixin.java
+++ src/main/java/net/threetag/palladium/mixin/client/SkyRendererMixin.java
@@ -1,7 +1,9 @@
 package net.threetag.palladium.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SkyRenderer;
+import net.threetag.palladium.client.renderer.WatcherRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,12 +12,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SkyRenderer.class)
 public class SkyRendererMixin {
 
-    @Inject(method = "renderSunMoonAndStars", at = @At("RETURN"))
+    @Inject(method = "initTextures", at = @At("RETURN"))
+    public void init(
+            CallbackInfo ci
+    ) {
+        WatcherRenderer.INSTANCE.init();
+    }
+
+    @Inject(method = "renderSunMoonAndStars", at = @At("HEAD"))
     public void renderSunMoonAndStars(
             PoseStack poseStack, float timeOfDay, int moonPhase, float rainLevel, float starBrightness, CallbackInfo ci
     ) {
-        // TODO
-//        WatcherRenderer.INSTANCE.render(poseStack, bufferSource, WatcherRenderer.INSTANCE.getVisibility(Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaTicks()));
+        WatcherRenderer.INSTANCE.render(poseStack, WatcherRenderer.INSTANCE.getVisibility(Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaTicks()));
     }
 
 }
