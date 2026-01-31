ad227edd53b523526b83932cf835c50f3a5e3fcb
Lucas Bandermann <lucas@threetag.net>
2026-01-31 12:57:49 +0100

    Added feature flag conditions to tailoring related recipes

--- src/main/java/net/threetag/palladium/flag/PalladiumFeatureFlags.java
+++ src/main/java/net/threetag/palladium/flag/PalladiumFeatureFlags.java
@@ -1,11 +1,18 @@
 package net.threetag.palladium.flag;
 
 import net.minecraft.world.flag.FeatureFlag;
+import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
+import net.neoforged.neoforge.common.conditions.FeatureFlagsEnabledCondition;
+import net.neoforged.neoforge.common.conditions.ICondition;
 import net.threetag.palladium.Palladium;
 
 public class PalladiumFeatureFlags {
 
     public static final FeatureFlag TAILORING = FeatureFlags.REGISTRY.getFlag(Palladium.id("tailoring"));
+
+    public static ICondition conditionTailoring() {
+        return new FeatureFlagsEnabledCondition(FeatureFlagSet.of(TAILORING));
+    }
 
 }
