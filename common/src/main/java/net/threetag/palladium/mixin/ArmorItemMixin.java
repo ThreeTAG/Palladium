89482961cbfe0b3cf6d33c115648e0d575a71744
Lucas Bandermann <lucas@threetag.net>
2026-02-03 18:43:41 +0100

    Fixed lock_armor ability not applying to right-clicking armor

--- common/src/main/java/net/threetag/palladium/mixin/ArmorItemMixin.java
+++ common/src/main/java/net/threetag/palladium/mixin/ArmorItemMixin.java
@@ -5,8 +5,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Equipable;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.threetag.palladium.power.ability.Abilities;
@@ -16,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Equipable.class)
-public interface EquipableMixin {
+@Mixin(ArmorItem.class)
+public class ArmorItemMixin {
 
-    @Inject(method = "swapWithEquipmentSlot", at = @At("HEAD"), cancellable = true)
-    private static void swapWithEquipmentSlot(Item item, Level level, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        ItemStack itemStack = player.getItemInHand(hand);
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void use(Level level, Player player, InteractionHand usedHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+        ItemStack itemStack = player.getItemInHand(usedHand);
         EquipmentSlot equipmentSlot = Mob.getEquipmentSlotForItem(itemStack);
 
         if (equipmentSlot.isArmor() && AbilityUtil.isTypeEnabled(player, Abilities.LOCK_ARMOR.get())) {
---
+++ common/src/main/java/net/threetag/palladium/mixin/ElytraItemMixin.java
@@ -0,0 +1,31 @@
+package net.threetag.palladium.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.threetag.palladium.power.ability.Abilities;
+import net.threetag.palladium.power.ability.AbilityUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ElytraItem.class)
+public class ElytraItemMixin {
+
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void use(Level level, Player player, InteractionHand usedHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+        ItemStack itemStack = player.getItemInHand(usedHand);
+        EquipmentSlot equipmentSlot = Mob.getEquipmentSlotForItem(itemStack);
+
+        if (equipmentSlot.isArmor() && AbilityUtil.isTypeEnabled(player, Abilities.LOCK_ARMOR.get())) {
+            cir.setReturnValue(InteractionResultHolder.fail(itemStack));
+        }
+    }
+
+}
