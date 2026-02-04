0ab70b57410dcbc891b9f90a5881f752adf2214a
Lucas Bandermann <lucas@threetag.net>
2026-02-04 15:00:50 +0100

    Fixes #182

--- common/src/main/java/net/threetag/palladium/mixin/OreFeatureMixin.java
+++ common/src/main/java/net/threetag/palladium/mixin/OreFeatureMixin.java
@@ -8,23 +8,37 @@ import net.threetag.palladium.PalladiumConfig;
 import net.threetag.palladium.block.PalladiumBlocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(OreFeature.class)
 public class OreFeatureMixin {
 
-    @ModifyArg(method = "doPlace", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunkSection;setBlockState(IIILnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;"), index = 3)
-    private BlockState injected(BlockState state) {
-        if (!PalladiumConfig.Server.REDSTONE_FLUX_CRYSTAL_GEODE_GENERATION.get()) {
-            return state;
-        } else if (state.is(Blocks.REDSTONE_ORE)) {
-            RandomSource random = RandomSource.create();
-            return random.nextInt(20) == 0 ? PalladiumBlocks.REDSTONE_FLUX_CRYSTAL_GEODE.get().defaultBlockState() : state;
-        } else if (state.is(Blocks.DEEPSLATE_REDSTONE_ORE)) {
-            RandomSource random = RandomSource.create();
-            return random.nextInt(15) == 0 ? PalladiumBlocks.DEEPSLATE_REDSTONE_FLUX_CRYSTAL_GEODE.get().defaultBlockState() : state;
-        } else {
-            return state;
+    @ModifyArgs(method = "doPlace", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunkSection;setBlockState(IIILnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;"))
+    private void injected(Args args) {
+        int blockStateIndex = -1;
+        for (int i = 0; i < args.size(); i++) {
+            if (args.get(i) instanceof BlockState) {
+                blockStateIndex = i;
+                break;
+            }
+        }
+
+        if (blockStateIndex != -1) {
+            BlockState state = args.get(blockStateIndex);
+            if (PalladiumConfig.Server.REDSTONE_FLUX_CRYSTAL_GEODE_GENERATION.get()) {
+                if (state.is(Blocks.REDSTONE_ORE)) {
+                    RandomSource random = RandomSource.create();
+                    if (random.nextInt(20) == 0) {
+                        args.set(blockStateIndex, PalladiumBlocks.REDSTONE_FLUX_CRYSTAL_GEODE.get().defaultBlockState());
+                    }
+                } else if (state.is(Blocks.DEEPSLATE_REDSTONE_ORE)) {
+                    RandomSource random = RandomSource.create();
+                    if (random.nextInt(15) == 0) {
+                        args.set(blockStateIndex, PalladiumBlocks.DEEPSLATE_REDSTONE_FLUX_CRYSTAL_GEODE.get().defaultBlockState());
+                    }
+                }
+            }
         }
     }
 
